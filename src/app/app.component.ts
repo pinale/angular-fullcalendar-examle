2167e422cb12407727e6b02b35ba565bc5d4e1b9
pinale <alessio.pinato@gmail.com>
2019-09-30 21:01:49 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent  {
   name = 'Angular';
   events = [
     { title: 'event 1', date: '2019-09-30' },
-    { title: 'event 2', date: '2019-10-01' }
+    { title: 'event 2', date: '2019-10-01' },
+    { title: 'event 3', date: '2019-10-02' }
 
   ];
 
